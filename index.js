4608c1c9bdb3536bc9f7a57320077c15132e6d66
Md. Shoyaif Rahman <sohan0020p@gmail.com>
2025-12-10 22:29:30 +0600

    review api

--- index.js
+++ index.js
@@ -97,6 +97,19 @@ async function run() {
       res.send(result);
     })
 
+     app.get('/reviews',async(req,res)=>{
+      const {foodId} = req.query
+      const query = {}
+      if(foodId){
+        query.foodId= foodId
+      }
+      const result = (await reviewCollection.find(query).sort({date:-1}).toArray())
+      res.send(result);
+    })
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
