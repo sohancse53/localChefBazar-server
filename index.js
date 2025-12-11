ee2560962cd729ce85448a36e4c0394290dd5b31
Md. Shoyaif Rahman <sohan0020p@gmail.com>
2025-12-11 15:41:30 +0600

    my order api created

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-require('dotenv').config();
-app.use(express.json())
+require("dotenv").config();
+app.use(express.json());
 app.use(cors());
 const port = process.env.PORT || 3000;
 
-
-
-
-
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hjbjloq.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,7 +15,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,126 +23,133 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const db = client.db('local_chef_bazar_db');
-    const userCollection = db.collection('users'); 
-    const mealCollection = db.collection('meals'); 
-    const reviewCollection = db.collection('reviews'); 
-    const favoriteCollection = db.collection('favorite'); 
-    const orderCollection = db.collection('orders'); 
-
+    const db = client.db("local_chef_bazar_db");
+    const userCollection = db.collection("users");
+    const mealCollection = db.collection("meals");
+    const reviewCollection = db.collection("reviews");
+    const favoriteCollection = db.collection("favorite");
+    const orderCollection = db.collection("orders");
 
     // ------------------------user related api--------------------------------
-    app.post('/users',async(req,res)=>{
-        const userInfo = req.body;
-        console.log(userInfo);
-        const email = userInfo.email;
-        userInfo.role = 'user';
-        userInfo.status = 'active';
-        userInfo.createdAt = new Date();  
-      
-        const userExist = await userCollection.findOne({email:email})
-        if(userExist){
-          res.send('user already exist');
-        }
-        const result = await userCollection.insertOne(userInfo)
-        res.send(result);
-    })
+    app.post("/users", async (req, res) => {
+      const userInfo = req.body;
+      console.log(userInfo);
+      const email = userInfo.email;
+      userInfo.role = "user";
+      userInfo.status = "active";
+      userInfo.createdAt = new Date();
 
-    app.get('/users',async(req,res)=>{
-        const cursor = userCollection.find()
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+      const userExist = await userCollection.findOne({ email: email });
+      if (userExist) {
+        res.send("user already exist");
+      }
+      const result = await userCollection.insertOne(userInfo);
+      res.send(result);
+    });
 
-
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     // ------------------------------meals related api-----------------------------------------
-  app.get('/meals', async (req, res) => {
-  const { sortOrder } = req.query; 
-  console.log('Sort order:', sortOrder);
+    app.get("/meals", async (req, res) => {
+      const { sortOrder } = req.query;
+      console.log("Sort order:", sortOrder);
 
-  const query = {};
-  let sortOption = {}; 
+      const query = {};
+      let sortOption = {};
 
-  if (sortOrder === 'Sort By Ascending') {
-    sortOption = { price: 1 }; // ascending
-  } else if (sortOrder === 'Sort By Descending') {
-    sortOption = { price: -1 }; // descending
-  }
+      if (sortOrder === "Sort By Ascending") {
+        sortOption = { price: 1 }; // ascending
+      } else if (sortOrder === "Sort By Descending") {
+        sortOption = { price: -1 }; // descending
+      }
 
-  const cursor = mealCollection.find(query).sort(sortOption);
-  const result = await cursor.toArray();
-  res.send(result);
-});
-
+      const cursor = mealCollection.find(query).sort(sortOption);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     // meals by id
-      app.get('/meal/:id',async(req,res)=>{
-          const id = req.params.id;
-          const query = {_id:new ObjectId(id)}
-        const result = await mealCollection.findOne(query);
-        res.send(result);
-    })
-
-
+    app.get("/meal/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await mealCollection.findOne(query);
+      res.send(result);
+    });
 
     // ---------------------------------reviews related Api----------------------------
-    app.post('/reviews',async(req,res)=>{
+    app.post("/reviews", async (req, res) => {
       const reviewInfo = req.body;
       reviewInfo.date = new Date();
       const result = await reviewCollection.insertOne(reviewInfo);
       res.send(result);
-    })
+    });
 
-     app.get('/reviews',async(req,res)=>{
-      const {foodId} = req.query
-      const query = {}
-      if(foodId){
-        query.foodId= foodId
+    app.get("/reviews", async (req, res) => {
+      const { foodId } = req.query;
+      const query = {};
+      if (foodId) {
+        query.foodId = foodId;
       }
-      const result = (await reviewCollection.find(query).sort({date:-1}).toArray())
+      const result = await reviewCollection
+        .find(query)
+        .sort({ date: -1 })
+        .toArray();
       res.send(result);
-    })
-
-
-
-
-
+    });
 
     //--------------------- favorite food related apis--------------------
 
-
-    app.post('/favorite-food',async(req,res)=>{
+    app.post("/favorite-food", async (req, res) => {
       const favoriteInfo = req.body;
-      const {userEmail ,foodId} = favoriteInfo; 
-      const exits = await favoriteCollection.findOne({userEmail,foodId});
-      if(exits){
-        return res.send('user already exits');
+      const { userEmail, foodId } = favoriteInfo;
+      const exits = await favoriteCollection.findOne({ userEmail, foodId });
+      if (exits) {
+        return res.send("user already exits");
       }
       favoriteInfo.addedTime = new Date();
-      const result= await  favoriteCollection.insertOne(favoriteInfo);
+      const result = await favoriteCollection.insertOne(favoriteInfo);
       res.send(result);
-    })
-
+    });
 
 
 
     // ------------------------------------order related apis-----------------------------------
 
-      app.post('/orders',async(req,res)=>{
-        const orderInfo = req.body;
-        const result = await orderCollection.insertOne(orderInfo);
-        res.send(result);
-      })
+    app.post("/orders", async (req, res) => {
+      const orderInfo = req.body;
+      const result = await orderCollection.insertOne(orderInfo);
+      res.send(result);
+    });
 
-
-
-
-
+    app.get("/orders", async (req, res) => {
+     const {userEmail} = req.query;
+     const query = {}
+     if(userEmail){
+      query.userEmail = userEmail;
+     }
+     const cursor = orderCollection.find(query).sort({orderTime:-1})
+     const result = await cursor.toArray();
+     res.send(result);
       
+      
+    });
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -155,24 +157,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.get('/',(req,res)=>{
-    res.send('hello hi bye')
-})
+app.get("/", (req, res) => {
+  res.send("hello hi bye");
+});
 
 app.listen(port, () => {
-  console.log(`listening on port ${port}`)
-})
-
+  console.log(`listening on port ${port}`);
+});
